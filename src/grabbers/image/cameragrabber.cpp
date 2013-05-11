c56cc6fdae1eeb495337d74c868a72b6933a63fd
Kirill Bukaev <kirill.bukaev@gmail.com>
2013-05-12 02:44:14 +0400

    CameraGrabber changes

--- src/grabbers/image/cameragrabber.cpp
+++ src/grabbers/image/cameragrabber.cpp
@@ -1,14 +1,40 @@
+/****************************************************************************
+**
+** QtMEL is a Qt Media Encoding Library that allows to encode video and audio streams
+** Copyright (C) 2013 Kirill Bukaev(aka KIBSOFT).
+** Contact: Kirill Bukaev (support@kibsoft.ru)
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this library; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**
+****************************************************************************/
+
 #include "cameragrabber.h"
 
 #include <QEventLoop>
 #include <QTimer>
 #include <QElapsedTimer>
 #include <QDateTime>
+
+#include <QCamera>
+
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 
 CameraGrabber::CameraGrabber(QObject *parent)
     : AbstractImageGrabber(parent)
+    , m_deviceIndex(-1)
     , m_initTime(1000)
 {
     connect(this, SIGNAL(stateChanged(AbstractGrabber::State)), this, SLOT(onStateChanged(AbstractGrabber::State)));
@@ -16,6 +42,18 @@ CameraGrabber::CameraGrabber(QObject *parent)
 
 CameraGrabber::~CameraGrabber()
 {
+}
+
+void CameraGrabber::setDeviceIndex(int index)
+{
+    if (m_deviceIndex != index) {
+        m_deviceIndex = index;
+    }
+}
+
+int CameraGrabber::deviceIndex() const
+{
+    return m_deviceIndex;
 }
 
 void CameraGrabber::setInitializationTime(int ms)
@@ -32,6 +70,9 @@ int CameraGrabber::initializationTime() const
 
 void CameraGrabber::setSize(const QSize &size)
 {
+    if (m_size != size) {
+        m_size = size;
+    }
 }
 
 QSize CameraGrabber::size() const
@@ -39,9 +80,23 @@ QSize CameraGrabber::size() const
     return m_size;
 }
 
+QStringList CameraGrabber::availableDeviceNames()
+{
+    QByteArray device;
+    QList<QByteArray> devices = QCamera::availableDevices();
+    QStringList names;
+
+    Q_FOREACH (device, devices) {
+        names.append(QCamera::deviceDescription(device));
+    }
+
+    return names;
+}
+
 bool CameraGrabber::start()
 {
-    m_camera = cvCreateCameraCapture(0);
+    if (!createCamera())
+        return false;
 
     QEventLoop initLoop;
     QTimer::singleShot(m_initTime, &initLoop, SLOT(quit()));
@@ -54,9 +109,25 @@ void CameraGrabber::onStateChanged(AbstractGrabber::State state)
 {
     if (state == AbstractGrabber::StoppedState
             || state == AbstractGrabber::SuspendedState) {
-        cvReleaseCapture(&m_camera);
-        m_camera = 0;
+        releaseCamera();
     }
+}
+
+bool CameraGrabber::createCamera()
+{
+    if (m_deviceIndex < 0 || m_deviceIndex > CameraGrabber::availableDeviceNames().count()) {
+        setError(AbstractGrabber::DeviceNotFoundError, tr("Device to be grabbed was not found."));
+        return false;
+    }
+    m_camera = cvCreateCameraCapture(m_deviceIndex);
+
+    return true;
+}
+
+void CameraGrabber::releaseCamera()
+{
+    cvReleaseCapture(&m_camera);
+    m_camera = 0;
 }
 
 QImage CameraGrabber::captureFrame()
--- src/grabbers/image/cameragrabber.h
+++ src/grabbers/image/cameragrabber.h
@@ -1,8 +1,32 @@
+/****************************************************************************
+**
+** QtMEL is a Qt Media Encoding Library that allows to encode video and audio streams
+** Copyright (C) 2013 Kirill Bukaev(aka KIBSOFT).
+** Contact: Kirill Bukaev (support@kibsoft.ru)
+**
+** This library is free software; you can redistribute it and/or
+** modify it under the terms of the GNU Lesser General Public
+** License as published by the Free Software Foundation; either
+** version 2.1 of the License, or (at your option) any later version.
+**
+** This library is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+** Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU Lesser General Public
+** License along with this library; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+**
+****************************************************************************/
+
 #ifndef CAMERAGRABBER_H
 #define CAMERAGRABBER_H
 
 #include "../../qtmel_global.h"
 #include "abstractimagegrabber.h"
+
+#include <QStringList>
 
 class CvCapture;
 
@@ -13,11 +37,16 @@ public:
     CameraGrabber(QObject *parent = 0);
     ~CameraGrabber();
 
+    void setDeviceIndex(int index);
+    int deviceIndex() const;
+
     void setInitializationTime(int ms);
     int initializationTime() const;
 
     void setSize(const QSize &size);
     QSize size() const;
+
+    static QStringList availableDeviceNames();
 
 public Q_SLOTS:
     bool start();
@@ -26,9 +55,12 @@ private Q_SLOTS:
     void onStateChanged(AbstractGrabber::State state);
 
 private:
+    bool createCamera();
+    void releaseCamera();
     QImage captureFrame();
 
     CvCapture *m_camera;
+    int m_deviceIndex;
     QSize m_size;
     int m_initTime;
 };
